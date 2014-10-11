decb2f06d0d23f7713a5a97cc5dd600786f9a58c
Kevin Prince <nextgengames@gmail.com>
2014-10-11 22:16:50 +0100

    Implementing adding a donation record and a message.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,12 +2,27 @@ class PaymentsController < ApplicationController
   def new
     @bt_client_token = Braintree::ClientToken.generate
     @payment = Donation.new
+    @list = List.find(params[:list_id])
   end
 
   def create
-    @result = Braintree::Transaction.sale(
+    Braintree::Transaction.sale(
       amount: params[:amount],
       payment_method_nonce: params[:payment_method_nonce]
     )
+
+    amount = params[:amount].to_f * 100
+
+    @payment = Donation.create(price: amount.to_i, list_id: params[:list_id])
+  end
+
+  def update
+    payment = Donation.find(params[:payment_id])
+    payment.message = params[:message]
+    payment.twitter_id = params[:twitter_id]
+    payment.save
+
+    #TODO: Add flash message final thankyou and go to actual list
+    redirect_to lists_path
   end
 end
