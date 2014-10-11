d7d197e116100c3949f0d61667bf9d073cab807b
Kevin Prince <nextgengames@gmail.com>
2014-10-11 19:16:12 +0100

    Creating new and create payment methods

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,12 @@
 class PaymentsController < ApplicationController
-  def show
+  def new
     @bt_client_token = Braintree::ClientToken.generate
+    @payment = Donation.new
   end
 
   def create
-    result = Braintree::Transaction.sale(
-      amount: '100.00',
+    @result = Braintree::Transaction.sale(
+      amount: params[:amount],
       payment_method_nonce: params[:payment_method_nonce]
     )
   end
