fe87ab15aee6a37af5b0be4e1beebb2eb81e793c
Ian Dow-Wright <ian@imagecircus.com>
2014-10-12 09:34:31 +0100

    added a telly to the products

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,47 +19,6 @@ list = List.create(
   description: "Yay, we're getting married!"
 )
 
-Product.create([
-  { name: "PlayStation 4",
-    price: 32986,
-    description: "PlayStation 4 redefines rich and immersive gameplay with powerful graphics and speed, intelligent personalisation, deeply integrated social capabilities, and innovative second-screen features
-",
-    image: "http://icons.iconarchive.com/icons/prepaidgamecards/gaming-gadgets/256/PlayStation-4-icon.png",
-    position: 1,
-    list: list
-  }
-])
-
-Product.create([
-  { name: "Wahl 2 Slice Toaster",
-    price: 1399,
-    description: "A great little toaster, cheap and cheerful, it does what it says on the box, it makes toast!!! Obviously not full of the features like the expensive ones, but hey toast is toast.",
-    image: "http://ecx.images-amazon.com/images/I/51l1aqA8bjL._SL1500_.jpg",
-    position: 1,
-    list_id: 2
-  }
-])
-
-Product.create([
-  { name: "Wahl 2 Slice Toaster",
-    price: 1399,
-    description: "A great little toaster, cheap and cheerful, it does what it says on the box, it makes toast!!! Obviously not full of the features like the expensive ones, but hey toast is toast.",
-    image: "http://ecx.images-amazon.com/images/I/51l1aqA8bjL._SL1500_.jpg",
-    position: 3,
-    list_id: 1
-  }
-])
-
-Product.create([
-  { name: "Tefal Intensium 28 cm Stirfry",
-    price: 1650,
-    description: "Tefal has invented a revolutionery new coating that last's up to 2x longer than most other non-stick cookware brands. This Intensium range comes with 9 layers of non-stick which possesses superior durability and resistant performance making this coating the most durable.",
-    image: "http://ecx.images-amazon.com/images/I/71pgyOY77zL._SL1500_.jpg",
-    position: 2,
-    list_id: 1
-  }
-])
-
 Donation.create([
   { price: 9999,
     message: "So so so happy for you",
@@ -120,3 +79,54 @@ list = List.create(
   title: 'Barack & Michelle',
   description: "Aren't we cute!"
 )
+
+Product.create([
+  { name: "PlayStation 4",
+    price: 32986,
+    description: "PlayStation 4 redefines rich and immersive gameplay with powerful graphics and speed, intelligent personalisation, deeply integrated social capabilities, and innovative second-screen features
+",
+    image: "http://icons.iconarchive.com/icons/prepaidgamecards/gaming-gadgets/256/PlayStation-4-icon.png",
+    position: 1,
+    list_id: 1
+  }
+])
+
+Product.create([
+  { name: "Wahl 2 Slice Toaster",
+    price: 1399,
+    description: "A great little toaster, cheap and cheerful, it does what it says on the box, it makes toast!!! Obviously not full of the features like the expensive ones, but hey toast is toast.",
+    image: "http://ecx.images-amazon.com/images/I/51l1aqA8bjL._SL1500_.jpg",
+    position: 1,
+    list_id: 2
+  }
+])
+
+Product.create([
+  { name: "Wahl 2 Slice Toaster",
+    price: 1399,
+    description: "A great little toaster, cheap and cheerful, it does what it says on the box, it makes toast!!! Obviously not full of the features like the expensive ones, but hey toast is toast.",
+    image: "http://ecx.images-amazon.com/images/I/51l1aqA8bjL._SL1500_.jpg",
+    position: 3,
+    list_id: 1
+  }
+])
+
+Product.create([
+  { name: "Tefal Intensium 28 cm Stirfry",
+    price: 1650,
+    description: "Tefal has invented a revolutionery new coating that last's up to 2x longer than most other non-stick cookware brands. This Intensium range comes with 9 layers of non-stick which possesses superior durability and resistant performance making this coating the most durable.",
+    image: "http://ecx.images-amazon.com/images/I/71pgyOY77zL._SL1500_.jpg",
+    position: 2,
+    list_id: 1
+  }
+])
+
+Product.create([
+  { name: "Samsung UE60H6240 60-inch 1080p",
+    price: 123200,
+    description: "Enjoy a faster and easier Smart TV experience, never miss your favourite shows and surround yourself with the action - with the huge screen 60 Samsung H6240 it's as good as being there - especially when the nation's favourite 3D experience5 can bring a new dimension to your favourite programmes.",
+    image: "http://ecx.images-amazon.com/images/I/91vYZx2MwxL._SL1500_.jpg",
+    position: 4,
+    list_id: 1
+  }
+])
